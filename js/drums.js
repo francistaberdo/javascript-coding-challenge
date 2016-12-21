9d54c078400d4f10a3a5e1c691912f7243f79055
Taberdz <robitaberdz@gmail.com>
2016-12-21 18:47:40 +0800

    This is my first commit for learning vanilla JavaScript

---
+++ js/drums.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const container = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return; // stop function from running
+    
+    
+    audio.currentTime = 0;
+    audio.play();
+    
+    container.classList.add('playing');
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach( key => key.addEventListener('transitionend', removeTransition));
